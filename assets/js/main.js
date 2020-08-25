3fc7f3d79bf57fafd6e3497aa90aa7fcbbbb213d
Yunus Emre Aydınlı <57762322+yunusemreaydinli@users.noreply.github.com>
2020-08-25 22:04:51 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,7 +31,7 @@
     /* ==========================================================================
     countdown timer
     ========================================================================== */
-     jQuery('#clock').countdown('2020/08/30',function(event){
+     jQuery('#clock').countdown('2020/09/05',function(event){
       var $this=jQuery(this).html(event.strftime(''
       +'<div class="time-entry days"><span>%-D</span> Days</div> '
       +'<div class="time-entry hours"><span>%H</span> Hours</div> '
